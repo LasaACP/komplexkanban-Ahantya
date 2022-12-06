3c994fa80b3e1ebec9f0c62c5fa612d97ea39b24
GonzNathan221 <100791330+NathanGonzalez2024@users.noreply.github.com>
2022-12-06 00:05:08 +0000

    yuran

---
+++ lib/Functions/ComplexReal.cpp
@@ -0,0 +1,17 @@
+/*
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Title : ComplexReal.cpp
+Author : Nathan Gonzalez
+URL : NA
+Description : The implementation of this function returns the real part of a complex number. Written by Nathan Gonzalez
+Created : Nov. 28th
+Modified : Dec. 5th
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+#include "../../include/Complex.h"
+
+double real(Complex& other)
+{
+  return other.real;
+}
